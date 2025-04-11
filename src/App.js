2517724f365c470df5693f48028a85de662b45e2
sindhuja15673 <132433742+sindhuja15673@users.noreply.github.com>
2025-04-11 16:01:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './components/register';
-import Login from './components/login';
+
 import Dashboard from './components/dashboard';
-import Home from './components/home';
+
 import AddJob from './components/addjob';
 import Profile from './components/profile';
 
@@ -13,9 +12,7 @@ const App = () => {
     <Router>
       <div>
         <Routes>
-          // <Route path='/' element={<Home />} />
-          // <Route path='/register' element={<Register />} />
-          // <Route path='/login' element={<Login />} />
+          
           <Route path='/' element={<Dashboard />} />
           <Route path='/add-job' element={<AddJob />} />
           <Route path='/profile' element={<Profile />} />
